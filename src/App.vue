752fe569688ab8c3cb98c5141c99d9eb2b305dbc
Sean Yang <seanyang0813@gmail.com>
2020-07-13 17:08:26 -0400

    cake app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="flex justify-center">
+    <div class="flex justify-center flex-col w-6/12">
+      <span class="m-8"></span>
+      <h1 class="text-center text-lg font-mono font-bold text-2xl">Is that a cake? Upload your image!</h1>
+      <div class="flex justify-center">
+        <input type="file" @change="onFileChange">      
+      </div>
+      <div v-if="url"  class="m-8 flex justify-center flex-col">
+        <img :src="url" class="object-center">
+        <p class="text-center" :class="{yes: finished}">{{result}}</p>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      url: null,
+      result: "...processing",
+      finished: false,
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
+      this.result = "...processing";
+      this.finished = false;
+      setTimeout(()=>{
+        this.result="YES";
+        this.finished = true;
+        }, 2000);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #image-box {
+    width: 50%;
+  }
+
+  .yes {
+    color: #42b883;
+    font-weight: bolder;
+    font-size: 5em;
+  }
 </style>
